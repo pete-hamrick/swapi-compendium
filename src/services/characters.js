8ddf5c07c0a7576e09df65b9cb083d204190c9f4
Pete Hamrick <pete.hamrick@gmail.com>
2021-12-01 19:58:18 -0800

    added useEffect hook for getting characters, midway through writing fetchCharacters function

---
+++ src/services/characters.js
@@ -0,0 +1,3 @@
+export const fetchCharacters = async () => {
+  const characterResults = await fetch(`https://swapi.dev/api/people/`)
+}
--- src/views/Compendium/Compendium.jsx
+++ src/views/Compendium/Compendium.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CharactersList from '../../components/CharactersList/CharactersList'
 
 export default function Compendium() {
   // add state hooks
+  const [loading, setLoading] = useState(false)
+  const [characters, setCharacters] = useState([])
 
   // add useEffect hooks
+  useEffect(() => {
+    async function getCharacters() {
+      const characterList = await fetchCharacters()
+      setCharacters(characterList)
+      setLoading(false)
+    }
+
+    getCharacters()
+  }, [])
 
   // any handlers?
+
+  if (loading) {
+    return <h1>Loading...</h1>
+  }
+
   return (
     <>
       <h1>SWAPI Compendium</h1>
