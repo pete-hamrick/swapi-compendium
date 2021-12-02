9baf3d04531a40c10c5795630b27d4e08eb8fc19
Pete Hamrick <pete.hamrick@gmail.com>
2021-12-02 10:16:05 -0800

    added basic CharacterList styling

--- src/components/CharactersList/CharactersList.jsx
+++ src/components/CharactersList/CharactersList.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Characters from '../Characters/Characters'
+import './CharactersList.css'
 
 export default function CharactersList({ characters }) {
   // const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   return (
-    <ul>
+    <ul className="character-container">
       {characters.map((character) => (
         <Characters key={character} character={character} />
       ))}
